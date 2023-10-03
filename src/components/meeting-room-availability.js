8ef12a4ed7b772486beb632bfb894fcf94cde48e
smarcet <smarcet@gmail.com>
2023-10-03 15:25:12 -0300

    Updated slot rendering logic * this is a temp solution till we add available slots pers room , from now on it will render only the available slots

--- src/components/meeting-room-availability.js
+++ src/components/meeting-room-availability.js
@@ -4,10 +4,31 @@ import T from "i18n-react";
 import {epochToMomentTimeZone} from "openstack-uicore-foundation/lib/utils/methods";
 
 export default ({date, availableDays, time_zone, availability, changeDate, onSelect}) => {
+
   const dateStr = date ? epochToMomentTimeZone(date, time_zone).format('Y-M-D') : '';
   const currentIndex = availableDays.findIndex(d => d.str === dateStr);
   const previousIndex = (currentIndex > 0) ? currentIndex - 1 : -1;
   const nextIndex = (currentIndex + 1 < availableDays.length) ? currentIndex + 1 : -1;
+
+  const renderSlots = () => {
+    return availability.data.length > 0 ? availability.data.map((a) => {
+      let isAvailable = a.status === 'Available';
+      let formatedStartTime = epochToMomentTimeZone(a.start_date, time_zone).format('HH:mm');
+      let formatedEndTime = epochToMomentTimeZone(a.end_date, time_zone).format('HH:mm');
+
+      if (isAvailable) {
+        return (
+          <div key={a.start_date} onClick={() => {
+            onSelect(a)
+          }} className={'meeting-room-availability-slot available'}>
+            {T.translate("bookable_room.available")}
+            <br/>
+            {formatedStartTime} - {formatedEndTime}
+          </div>
+        );
+      }
+    }) : <p>{T.translate("bookable_room.not_available_slots")}</p>;
+  }
 
   return (
     <div className={'meeting-room-availability'}>
@@ -23,33 +44,9 @@ export default ({date, availableDays, time_zone, availability, changeDate, onSel
         </div>
       </div>
       <div className={'meeting-room-availability-body col-xs-12'}>
-        {availability && availability.data !== null ?
-          availability.data.map((a) => {
-            let isAvailable = a.status === 'Available';
-            let formatedStartTime = epochToMomentTimeZone(a.start_date, time_zone).format('HH:mm');
-            let formatedEndTime = epochToMomentTimeZone(a.end_date, time_zone).format('HH:mm');
-
-            if (isAvailable) {
-              return (
-                <div key={a.start_date} onClick={() => {
-                  onSelect(a)
-                }} className={'meeting-room-availability-slot available'}>
-                  {T.translate("bookable_room.available")}
-                  <br/>
-                  {formatedStartTime} - {formatedEndTime}
-                </div>
-              );
-            } else {
-              return (
-                <div key={a.start_date} className={'meeting-room-availability-slot unavailable'}>
-                  {T.translate("bookable_room.unavailable")}
-                  <br/>
-                  {formatedStartTime} - {formatedEndTime}
-                </div>
-              );
-            }
-
-          }) : null}
+        {
+          availability && availability.data !== null ? renderSlots() : null
+        }
       </div>
     </div>
   );
